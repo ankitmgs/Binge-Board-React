21112937502076f9bac9183e128064a44cfb70bf
“Ankit <guptankit753@gmail.com>
2025-09-10 08:16:00 +0530

    Fix: Update BACKEND_BASE_URL to use production URL

--- src/constant/apiUrl.tsx
+++ src/constant/apiUrl.tsx
@@ -3,7 +3,7 @@ export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 export const TMDB_BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/w1280";
 export const TMDB_PROVIDER_LOGO_BASE_URL = "https://image.tmdb.org/t/p/w92";
 
-export const BACKEND_BASE_URL = "http://localhost:5000/api";
+export const BACKEND_BASE_URL = "https://binge-board-backend.onrender.com/api";
 export const LISTURL = `${BACKEND_BASE_URL}/lists`;
 export const AddItemTOListURL = `${BACKEND_BASE_URL}/lists/addItemToLists`;
 export const AllItemIDsURL = `${BACKEND_BASE_URL}/lists/allItemIds`;
